cd8118354aebdf28a5caa96a82f5c873e7bd3d6c
siddheshsurve <sysurve24@gmail.com>
2022-08-26 18:54:08 +0530

    Given style to main page

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,16 @@
 import React from "react";
-
+import "../index.css"
 export default function Main () {
     return (
-        <h1>Here is main content</h1>
+        <main>
+            <h1 className="main-h1">Fun facts about React</h1>
+            <ul className="main-ul">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on Github</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
     )
 }
